8584f2f49488e27de1a07d253bfacd999a8c7a9d
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-26 22:29:24 -0500

    add: javabean for TipoResiduo table on DB

---
+++ src/main/java/model/TipoResiduo.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+public class TipoResiduo implements Serializable {
+
+    private int residuoId;
+    private String tipoResiduo;
+
+    public TipoResiduo() {
+
+    }
+
+    public TipoResiduo(int residuoId, String tipoResiduo) {
+        setResiduoId(residuoId);
+        setTipoResiduo(tipoResiduo);
+    }
+
+    public int getResiduoId() {
+        return residuoId;
+    }
+
+    public void setResiduoId(int residuoId) {
+        this.residuoId = residuoId;
+    }
+
+    public String getTipoResiduo() {
+        return tipoResiduo;
+    }
+
+    public void setTipoResiduo(String tipoResiduo) {
+        this.tipoResiduo = (tipoResiduo != null) ? tipoResiduo : "";
+    }
+}
