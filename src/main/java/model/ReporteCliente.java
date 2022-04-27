f94a3d5dc7b39046a1b01d89c9e7b2bfc461712d
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-26 22:29:02 -0500

    add: javabean for ReporteCliente table on DB

---
+++ src/main/java/model/ReporteCliente.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.io.Serializable;
+
+public class ReporteCliente implements Serializable {
+
+    public long folio;
+    private long fkCliente;
+    private long fkTipoResiduo;
+    private long fkMetodoPago;
+    private boolean pagado;
+
+
+    public ReporteCliente() {
+
+    }
+
+    public ReporteCliente(long folio, long fkCliente, long fkTipoResiduo, long fkMetodoPago, boolean pagado) {
+        this.folio = folio;
+        this.fkCliente = fkCliente;
+        this.fkTipoResiduo = fkTipoResiduo;
+        this.fkMetodoPago = fkMetodoPago;
+        this.pagado = pagado;
+    }
+
+    public long getFolio() {
+        return folio;
+    }
+
+    public void setFolio(long folio) {
+        this.folio = (folio < 0) ? 0 : folio;
+    }
+
+    public long getFkCliente() {
+        return fkCliente;
+    }
+
+    public void setFkCliente(long fkCliente) {
+        this.fkCliente = (fkCliente <= 0) ? 1 : fkCliente;
+    }
+
+    public long getFkTipoResiduo() {
+        return fkTipoResiduo;
+    }
+
+    public void setFkTipoResiduo(long fkTipoResiduo) {
+        this.fkTipoResiduo = (fkTipoResiduo <= 0) ? 1 : fkTipoResiduo;
+    }
+
+    public long getFkMetodoPago() {
+        return fkMetodoPago;
+    }
+
+    public void setFkMetodoPago(long fkMetodoPago) {
+        this.fkMetodoPago = (fkMetodoPago <= 0) ? 1 : fkMetodoPago;
+    }
+
+    public boolean getPagado() {
+        return pagado;
+    }
+
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+}
