e044b81ef1e2517fbe4d41fce899fad2141468cc
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-05-26 23:25:30 -0500

    mod: class now implements Serializable interface

--- src/main/java/model/Direccion.java
+++ src/main/java/model/Direccion.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Direccion {
+import java.io.Serializable;
+
+public class Direccion implements Serializable {
 
     private String codigoPostal;
     private String colonia;
