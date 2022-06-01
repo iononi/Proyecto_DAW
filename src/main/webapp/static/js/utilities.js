e73c482c35125139ee05d4607f386fbfbae18520
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-05-31 22:18:30 -0500

    add: code to show/close popup message

---
+++ src/main/webapp/static/js/utilities.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("pop-up");
+
+openPopup = () => {
+    popup.classList.add("open-popup");
+}
+
+closePopup = () => {
+    popup.classList.remove("open-popup");
+}
