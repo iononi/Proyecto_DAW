91b414cdd676f141a17707ba248c7feafc4c16d7
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-06-01 23:28:46 -0500

    add: logic for users report query

--- src/main/java/controller/ReportServlet.java
+++ src/main/java/controller/ReportServlet.java
@@ -7,13 +7,16 @@ import data.dao.TipoResiduoDao;
 import model.Direccion;
 import model.ReporteAnonimo;
 import model.ReporteCliente;
+import model.Usuario;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.LinkedList;
 
-@WebServlet(name = "ReportServlet", value = {"/clientReport", "/anonymousReport", "/anonymousReportRequest", "/clientReportRequest"})
+@WebServlet(name = "ReportServlet", value = {"/clientReport", "/anonymousReport", "/anonymousReportRequest",
+        "/clientReportRequest", "/searchByStatus"})
 public class ReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,21 +40,21 @@ public class ReportServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        int tipoResiduo = Integer.parseInt( request.getParameter("tipoResiduo") ),
-                metodoPago = Integer.parseInt( request.getParameter("metodoPago") );
-        short status = Short.parseShort( request.getParameter("status") );
-        boolean estaPagado = !(metodoPago == 3); // if equals 3 then it'll be paid with cash, then is not paid at
-                                                // insertion
 
+        ReporteClienteDao myClientReport = new ReporteClienteDao();
         switch ( request.getServletPath() ) {
             case "/clientReport":
+                int tipoResiduo = Integer.parseInt( request.getParameter("tipoResiduo") ),
+                        metodoPago = Integer.parseInt( request.getParameter("metodoPago") );
+                short status = Short.parseShort( request.getParameter("status") );
+                boolean estaPagado = !(metodoPago == 3); // if equals 3 then it'll be paid with cash, then is not paid at
+                // insertion
                 // may insert on ReporteCliente on database
                 int fkCliente = Integer.parseInt( request.getParameter("usuarioId") ),
                         fkTipoResiduo = Integer.parseInt( request.getParameter("tipoResiduo") ),
                         fkMetodoPago = Integer.parseInt( request.getParameter("metodoPago") );
                 short fkEstado = Short.parseShort( request.getParameter("status") );
 
-                ReporteClienteDao myClientReport = new ReporteClienteDao();
                 ReporteCliente clientReport = new ReporteCliente(fkCliente, fkTipoResiduo, fkMetodoPago, estaPagado,
                         fkEstado);
 
@@ -66,6 +69,11 @@ public class ReportServlet extends HttpServlet {
                 break;
 
             case "/anonymousReport":
+                tipoResiduo = Integer.parseInt( request.getParameter("tipoResiduo") );
+                metodoPago = Integer.parseInt( request.getParameter("metodoPago") );
+                status = Short.parseShort( request.getParameter("status") );
+                estaPagado = !(metodoPago == 3); // if equals 3 then it'll be paid with cash, then is not paid at
+                // insertion
                 // may insert on ReporteAnonimo on database
                 String nombre = request.getParameter("nombre"),
                         apellidop = request.getParameter("apellidop"),
@@ -96,6 +104,35 @@ public class ReportServlet extends HttpServlet {
                     response.sendRedirect("./index.jsp");
                 }
 
+                break;
+
+            case "/searchByStatus":
+                String folio = request.getParameter("folio"),
+                        statusReport = request.getParameter("status");
+                Usuario user = (Usuario) request.getSession().getAttribute("currentUser");
+                if ( folio.equals("") ) { // search by status
+                    LinkedList<ReporteClienteDao.User> userList = myClientReport.searchByStatus(user.getClienteId(),
+                            Short.parseShort(statusReport));
+                    if (!userList.isEmpty())
+                        request.getSession().setAttribute("userReportList", userList);
+                    else {
+                        request.getSession().setAttribute("userReportList", null);
+                        request.getSession().setAttribute("showPopupMessage", true);
+                        request.getSession().setAttribute("searchType","estado");
+                    }
+                    response.sendRedirect("views/user/profile.jsp");
+                } else { // search by folio
+                    myClientReport.select(Integer.parseInt(folio));
+                    LinkedList<ReporteClienteDao.User> report = myClientReport.getAuxUser();
+                    if ( report == null || report.isEmpty() ) {
+                        request.getSession().setAttribute("userReportList", null);
+                        request.getSession().setAttribute("showPopupMessage", true);
+                        request.getSession().setAttribute("searchType", "folio");
+                    } else {
+                        request.getSession().setAttribute("userReportList", report);
+                    }
+                    response.sendRedirect("views/user/profile.jsp");
+                }
                 break;
         }
     }
